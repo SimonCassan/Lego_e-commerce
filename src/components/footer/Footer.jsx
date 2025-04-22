6ca62fc7dd198ae35e82357b0e46d13193be0804
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-22 12:21:44 +0200

    Initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p>&#xA9; 2025 Vente de Lego. Tous droits réservés.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Vente de Logo</h1>
+        </header>
+    );
+}
+
+export default Header;
