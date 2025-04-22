fc75d9ea84ecc0a6013230719b42eae05ae39d96
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-22 16:58:13 +0200

    Add available prop

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,16 +1,18 @@
 import './card.css'
 
-function Card({ src, name, tag, price }) {
+function Card({ src, name, tag, price, available }) {
+    if (!available) return null;
     return (
-        <div className="card">
+        <div className="card" >
             <img src={src} alt={`Illustration ${name}`} />
             <div className='card-content'>
                 <h2>{name}</h2>
                 <p className="card-tag">{tag}</p>
                 <p>{price}€</p>
+                <p>{available ? "Disponible" : "En rupture"}</p>
                 <a className="card-btn" href="#">Ajouter</a>
             </div>
-        </div>
+        </div >
     );
 }
 
