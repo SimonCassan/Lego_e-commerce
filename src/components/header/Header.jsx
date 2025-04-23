19eb9d5f8deb5155d794bddeb7dba72cf7c80066
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-23 15:56:42 +0200

    Improve header visuals

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import './header.css';
 
-function Header() {
+function Header({ toggleCart }) {
     return (
         <header className="header">
-            <h1>Vente de Lego</h1>
+            <div className="logo">
+                <img src="../../public/img/logo.png" alt="Lego Brick logo" /><h1>Vente de Lego</h1>
+            </div>
+            <button className="shoppingCart" onClick={toggleCart}><img src="../../public/img/shopping-cart.png" alt="Shopping cart icon" /></button>
         </header>
     );
 }
