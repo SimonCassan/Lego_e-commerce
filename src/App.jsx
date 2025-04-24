d4cd46d50567f425bea49485453a4f3e2d8b0003
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-24 12:04:08 +0200

    Add quantity to cart and cart numbers to header

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,31 @@ import { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
+
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product])
+    setCart((prevCart) => {
+      const itemIndex = prevCart.findIndex(p => p.id === product.id);
+      const updatedCart = [...prevCart];
+      if (itemIndex !== -1)
+        updatedCart[itemIndex].quantity += 1;
+      else
+        updatedCart.push({ ...product, quantity: 1 });
+      return updatedCart;
+    })
   };
+
   const removeFromCart = indexToRemove => {
-    setCart((prevCart) => prevCart.filter((item, index) =>
-      index !== indexToRemove
-    ));
+    setCart((prevCart) => {
+      const updatedCart = [...prevCart];
+      if (updatedCart[indexToRemove].quantity > 1)
+        updatedCart[indexToRemove].quantity -= 1;
+      else
+        updatedCart.splice(indexToRemove, 1);
+      return updatedCart;
+    });
   }
-  const total = cart.reduce((sum, product) => sum + product.price, 0);
+
+  const total = cart.reduce((sum, product) => sum + product.price * product.quantity, 0);
 
   const [isCartVisible, setIsCartVisible] = useState(false);
   const toggleCart = () => {
@@ -25,7 +41,7 @@ function App() {
 
   return (
     <>
-      <Header toggleCart={toggleCart} />
+      <Header toggleCart={toggleCart} cart={cart} />
       <main>
         <CardsList addToCart={addToCart} />
         {isCartVisible && <Cart products={cart} removeFromCart={removeFromCart} total={total} />}
