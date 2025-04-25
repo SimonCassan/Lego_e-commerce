4020d51de30b295371f909128802b43c4b1d6cd6
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-25 15:41:09 +0200

    Add Search input

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import './header.css';
+import Search from '../Search/Search';
 
-function Header({ toggleCart, cart }) {
+function Header({ toggleCart, cart, onSearchChange }) {
     return (
         <header className="header">
             <div className="logo">
                 <img src="/img/logo.png" alt="Lego Brick logo" /><h1>Vente de Lego</h1>
             </div>
-            <button className="shoppingCart" onClick={toggleCart} data-count={"(" + cart.reduce((acc, p) => acc + p.quantity, 0) + ")"}><img src="/img/shopping-cart.png" alt="Shopping cart icon" /></button>
+            <div className='header-right'>
+                <Search onSearchChange={onSearchChange} />
+                <button className="shoppingCart" onClick={toggleCart} data-count={cart.reduce((acc, p) => acc + p.quantity, 0)}><img src="/img/shopping-cart.png" alt="Shopping cart icon" /></button>
+            </div>
         </header >
     );
 }
