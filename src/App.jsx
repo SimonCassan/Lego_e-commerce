bb406078015314bea823e6f1b6967b02b768b007
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-25 15:38:29 +0200

    Fix typo - add search/category

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer";
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer";
 import CardsList from "./components/CardsList/CardsList";
 import Cart from "./components/Cart/Cart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import CheckBox from "./components/Checkbox/Checkbox";
+import Select from "./components/Select/Select";
 
 
 function App() {
@@ -39,11 +41,47 @@ function App() {
     setIsCartVisible(prev => !prev);
   };
 
+  const [showAvailable, setShowAvailable] = useState(false);
+  const toggleAvailable = () => {
+    setShowAvailable(prev => !prev)
+  }
+
+  const [category, setCategory] = useState("Tous");
+  const switchCategory = (value) => {
+    setCategory(() => value);
+  };
+
+  const [products, setProducts] = useState([]);
+  const [tags, setTags] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/src/data/products.json');
+        const result = await response.json();
+        setProducts(result);
+
+        const uniqueTags = [...new Set(result.map(product => product.tag))];
+        setTags(uniqueTags);
+
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredProducts = products.filter(product =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
-      <Header toggleCart={toggleCart} cart={cart} />
+      <Header toggleCart={toggleCart} cart={cart} onSearchChange={setSearchTerm} />
       <main>
-        <CardsList addToCart={addToCart} />
+        <CheckBox toggleAvailable={toggleAvailable} />
+        <Select onChange={switchCategory} tagsList={tags} />
+        <CardsList addToCart={addToCart} showAvailable={showAvailable} showCategory={category} productsList={products} searchProducts={filteredProducts} />
         {isCartVisible && <Cart products={cart} removeFromCart={removeFromCart} total={total} />}
       </main>
       <Footer />
