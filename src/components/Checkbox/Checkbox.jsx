7833fd6f6cc3fbe5747c6af222c50c008ef11039
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-25 15:41:35 +0200

    Add checkbox logic

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,5 +1,7 @@
-function CheckBox() {
-    return <label><input type="checkbox" name="" /></label>
+import "./checkbox.css"
+
+function CheckBox({ toggleAvailable }) {
+    return <label className="checkbox">N'afficher que les éléments disponibles<input type="checkbox" name="checkAvailable" onChange={toggleAvailable} /></label>
 }
 
 export default CheckBox;
