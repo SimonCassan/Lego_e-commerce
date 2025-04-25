d5f51635c99f55dcc7f4675617334f56b6afb7df
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-25 15:40:33 +0200

    Add proper filtering

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,14 +1,22 @@
 import "./cardslist.css";
 
 import Card from "../Cards/Card";
-import products from "../../data/products";
 
-function CardsList({ addToCart }) {
+function CardsList({ addToCart, showAvailable, showCategory, productsList, searchProducts }) {
+    const productsToDisplay = searchProducts.length > 0 ? searchProducts : productsList;
+
     return (
         <div className="cards-list">
-            {products.map((product) =>
-                <Card key={product.id} product={product} addToCart={addToCart} />
-            )}
+            {productsToDisplay
+                .filter(product => {
+                    const matchAvailability = product.available || !showAvailable;
+                    const matchCategory = showCategory === "Tous" || product.tag === showCategory;
+
+                    return matchAvailability && matchCategory;
+                })
+                .map(product => (
+                    <Card key={product.id} product={product} addToCart={addToCart} />
+                ))}
         </div>
     );
 }
